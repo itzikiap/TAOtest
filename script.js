a0e22defa9138461512471a985c049d4995d700b
itzikiap <itzikiap@gmail.com>
2018-08-25 03:39:48 +0300

    Added some design and clearing of code Created a parsing function

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-function searchNames(nameMask) {
-	$.getJSON("https://hr.oat.taocloud.org/v1/users?name="+nameMask, function(json) {
-		$("#dialog").dialog("open");
-		$("#dialog").dialog( "instance" ).element[0].innerText = JSON.stringify(json);
+function searchNames(nameMask, listParserCallback) {
+	$.getJSON("https://hr.oat.taocloud.org/v1/users?limit=200&name="+nameMask, function(json) {
+		
+		$("#content").dialog("open");
+		$("#content").dialog("option", "title", "Showing: " + (nameMask!=""?nameMask:"<all>") );
+		var listData = listParserCallback ? listParserCallback(json) : defaultParser(json);
+		var listString = "";
+		for (l in listData) {
+			var item = listData[l];
+			listString += '<li >'+item+'</li>';
+		}
+		
+		$("#content").html('<ul id="namesList" class="demo-list">'+listString+'</ul>');
 	});
 }
 
+function defaultParser(json) {
+	var listData = [];
+	for  (l in json) {
+		var item = json[l];
+		listData.push(item.firstName+" "+item.lastName);
+	}
+	return listData;
+}
+
 $( function() {
-	$( "#dialog" ).dialog({
+	$( "#content" ).dialog({
 		  autoOpen: false,
-		  title: "JSON results"
+		  draggable: false,
+		  position: { my: "left top", at: "left top", of: window },
+		  hide: { effect: "fold", duration: 500 },
+		  show: { effect: "fold", duration: 500 }
 	});
 } );
