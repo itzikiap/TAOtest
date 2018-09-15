416612035249c9d43b8623ae0f61ba48e9096022
itzikiap <itzikiap@gmail.com>
2018-09-16 00:43:46 +0300

    Added error handling Rebuilt the list a bit more correctly

--- namesListDialog.js
+++ namesListDialog.js
@@ -1,16 +1,31 @@
 function searchNames(nameMask, listParserCallback) {
-	$.getJSON("https://hr.oat.taocloud.org/v1/users?limit=200&name="+nameMask, function(json) {
+	$.getJSON("https://hr.oat.taocloud.org/v1/users?limit=200&name="+nameMask)
+	.done(function(json) {
 		
 		$("#content").dialog("open");
 		$("#content").dialog("option", "title", "Showing: " + (nameMask!=""?nameMask:"<all>") );
 		var listData = listParserCallback ? listParserCallback(json) : defaultParser(json);
 		var listString = "";
+		var items = [];
+		
+		$.each( listData, function( key, val ) {
+			items.push( "<li id='" + key + "'>" + val.val + "</li>" );
+		});
+		
+		var listItems = $( "<ul/>", {
+			"class": "demo-list",
+			html: items.join( "" )
+		}).appendTo($("#content"));
+		
+		
 		for (l in listData) {
 			var item = listData[l];
 			listString += '<li >'+item+'</li>';
 		}
-		
-		$("#content").html('<ul id="namesList" class="demo-list">'+listString+'</ul>');
+	})
+	.fail(function(json) {
+		$("#content").dialog("open");
+		$("#content").html('<div>Failed loading the list. Reopen the dialog to retry.</div>');
 	});
 }
 
@@ -18,7 +33,7 @@ function defaultParser(json) {
 	var listData = [];
 	for  (l in json) {
 		var item = json[l];
-		listData.push(item.firstName+" "+item.lastName);
+		listData.push({val: item.firstName+" "+item.lastName});
 	}
 	return listData;
 }
